96f3ccfa3b759c96a051e59e7f2331d42abcd808
fa-andy <andy.liang@fa-software.com>
2021-05-23 23:24:38 +0800

    Golang lesson 1

---
+++ lilac-golang/go.mod
@@ -0,0 +1,3 @@
+module softquire.com/hello
+
+go 1.16
---
+++ lilac-golang/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	const HELLO = "Hello, World!"
+	fmt.Println(HELLO)
+	log.Print(HELLO)
+}
